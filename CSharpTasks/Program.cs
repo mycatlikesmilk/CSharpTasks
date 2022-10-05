2c8ef0d023da7835eed02c4f7970c81d8773c891
TheNexoOne <anatoliy.vyskub@gmail.com>
2022-10-05 03:11:34 +0300

    Сделал задачу №3

--- CSharpTasks/Program.cs
+++ CSharpTasks/Program.cs
@@ -15,7 +15,20 @@
 
         static void Main(string[] args)
         {
+            Console.WriteLine("Запишите два числа через пробел: ");
+            string a = Console.ReadLine();
 
+
+            string[] counts = a.Split(' ');
+
+            int number = Int32.Parse(counts[0]);
+            int number2 = Int32.Parse(counts[1]);
+            int sum = number + number2;
+
+            Console.WriteLine("Сумма введённых чисел ");
+            Console.WriteLine($"{sum}");
+
+            Console.ReadLine();
         }
     }
 }
