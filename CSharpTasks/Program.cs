5d958c8d86414b2fdc1794bfcc1c42775990f2a7
TheNexoOne <anatoliy.vyskub@gmail.com>
2022-10-05 03:06:44 +0300

    Сделал задачу №2

--- CSharpTasks/Program.cs
+++ CSharpTasks/Program.cs
@@ -19,7 +19,21 @@
 
         static void Main(string[] args)
         {
+            Console.WriteLine("Введите текст: ");
+            var text = Console.ReadLine();
 
+            var height = Console.WindowHeight;
+            var witdth = Console.WindowWidth;
+
+            var textLength = text.Length;
+            Console.WriteLine($"{textLength}");
+            Console.WriteLine($"{witdth}");
+
+            var center = (witdth / 2) + (textLength / 2);
+
+            Console.WriteLine(text.PadLeft(center));
+
+            Console.ReadLine();
         }
     }
 }
---
+++ CSharpTasks/Task 2.cs
@@ -0,0 +1,25 @@
+﻿namespace Tasks.Task2
+{
+    internal class Program
+    {
+        public static void Task2()
+        {
+
+            Console.WriteLine("Введите текст: ");
+            var text = Console.ReadLine();
+
+            var height = Console.WindowHeight;
+            var witdth = Console.WindowWidth;
+
+            var textLength = text.Length;
+            Console.WriteLine($"{textLength}");
+            Console.WriteLine($"{witdth}");
+
+            var center = (witdth / 2) + (textLength / 2);
+
+            Console.WriteLine(text.PadLeft(center));
+
+            Console.ReadLine();
+        }
+    }
+}
